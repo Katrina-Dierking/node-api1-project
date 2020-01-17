dc6e7c0ef4c746fb9f2150552124fd6d26c0618b
Katrina_Dierking <kalynndier@gmail.com>
2020-01-16 21:10:49 -0500

    testing API endpoints

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const db = require ('./data/seeds/users.js');
 const express = require('express');
 
 const server = express();
+server.use(express.json());
 
 server.listen(4000, () => {
     console.log('=== server listening on port 4000 ===');
